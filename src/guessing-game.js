a7d1accb26c82213c06723d3fe4ebc15414b081e
pashakiz <pashakiz@gmail.com>
2021-09-02 22:03:14 +0300

    feat: implement class methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
+      if (min > max) throw Error ("Error: min can't be bigger than max.")
+      this.min = min
+      this.max = max
+    }
 
+    findMiddle() {
+      return Math.round((this.max + this.min) / 2)
     }
 
     guess() {
-
+      return this.findMiddle()
     }
 
     lower() {
-
+      this.max = this.findMiddle()
     }
 
     greater() {
-
+      this.min = this.findMiddle()
     }
 }
 
